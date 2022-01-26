39bcf54a44d0d0a5d68efa30510b8782595b4f87
deepumoses <sonicdeepak@gmail.com>
2022-01-26 14:29:02 +0530

    feat(problem): alternate and better readable and understandable count unique values solution

--- Problems/Arrays/countUniqueValues.js
+++ Problems/Arrays/countUniqueValues.js
@@ -1,35 +1,54 @@
 // Given a sorted array count the number of unique values in it
 // Restriction:  Space should be O(1)
+// PATTERN: MULTIPLE POINTERS
 
-function countUniqueValues(arr){
-    // edge case
-    if (!arr.length) return 0;
-    
-    let i = 0;
-    let j = i + 1;
-    
-    while (j < arr.length) {
-        const left = arr[i];
-        const right = arr[j];
-        
-        if (left === right) {
-            j++;
-        } else {
-            if (j-i > 1) {
-                i++;
-                arr[i] = right;
-                j++;
-            } else {
-                i++;
-                j++;
-            }
-        }
+// for loop (more readable and easier to understand)
+function countUniqueValuesFor(arr) {
+  let i = 0;
+
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
     }
-    
-    return i+1; // coz arrays are zero indexed
   }
 
-const result = countUniqueValues([-3, -1, 1, 5, 6, 7, 7, 8, 8, 8, 11]);
-console.log(result);
+  return i === 0 ? i : i + 1;
+}
 
-  // Pattern used: MultiplePointers
+// using while loop
+function countUniqueValuesWhile(arr) {
+  // edge case
+  if (!arr.length) return 0;
+
+  let i = 0;
+  let j = i + 1;
+
+  while (j < arr.length) {
+    const left = arr[i];
+    const right = arr[j];
+
+    if (left === right) {
+      j++;
+    } else {
+      if (j - i > 1) {
+        i++;
+        arr[i] = right;
+        j++;
+      } else {
+        i++;
+        j++;
+      }
+    }
+  }
+
+  return i + 1; // coz arrays are zero indexed
+}
+
+// for loop
+const result1 = countUniqueValuesFor([-3, -1, 1, 5, 6, 7, 7, 8, 8, 8, 11]);
+console.log(result1);
+
+// while loop
+const result2 = countUniqueValuesWhile([-3, -1, 1, 5, 6, 7, 7, 8, 8, 8, 11]);
+console.log(result2);
